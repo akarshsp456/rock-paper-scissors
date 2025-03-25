0e9359c4ccebc515cfc8709fd26fae54bef0dd90
Akarsh <akarshsp456@gmail.com>
2025-03-25 12:17:40 +0530

    Add playRound function to decide the logic of game

--- script.js
+++ script.js
@@ -22,5 +22,49 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "stone"){
+        if (computerChoice === "stone"){
+            console.log("It's a tie!");
+        }
+        else if (computerChoice === "paper"){
+            console.log("You lose :(  paper beats stone!");
+            computerScore++ ;
+        }    
+        else{
+            console.log("You win! stone beats scissor!");
+            humanScore++ ;
+        }
+    }
+
+    else if (humanChoice === "paper"){
+        if (computerChoice === "stone"){
+                console.log("You win! paper beats stone!");
+                humanScore++ ;
+        }
+        else if (computerChoice === "paper"){
+                console.log("It's a tie");
+        }    
+        else{
+                console.log("You lose :(  scissors beats paper!");
+                computerScore++ ;
+        }
+    }
+
+    else if (humanChoice === "scissors"){
+        if (computerChoice === "stone"){
+            console.log("You lose :(  stone beats scissors!");
+            computerScore++ ;
+        }
+        else if (computerChoice === "paper"){
+            console.log("You win! scissors beats paper");
+            humanScore++ ;
+        }    
+        else{
+            console.log("It's a tie");
+        }
+    }    
+
+}
